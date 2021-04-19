ccebbc00969b36f900e5b33323a998e1deaa3704
vinita1005 <vinitachapper@gmail.com>
2021-04-19 17:36:27 -0400

    Baseline code added

---
+++ src/test/java/com/webservice/kanban/KanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webservice.kanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
